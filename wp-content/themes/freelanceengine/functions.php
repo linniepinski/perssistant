f098de12eb588e091e9e0a728a9272c1cfe7000c
root <root@mail.ukrosoft.com.ua>
2016-03-14 18:45:03 +0200

    [138] 83412426 : perssistant.demo.ukrosoft.com.ua/contact/

--- wp-content/themes/freelanceengine/functions.php
+++ wp-content/themes/freelanceengine/functions.php
@@ -3005,9 +3005,4 @@ function upLoadCvUser(){
 	exit;
 }
 
-function append_query_string($url) {
-    return add_query_arg('?r=1', $url);
-}
-add_filter('the_permalink', 'append_query_string');
-
 include 'includes/price_range.php';
