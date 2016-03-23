381a546cf9b6cb871632ccc4c46bd5cfbbceba66
root <root@mail.ukrosoft.com.ua>
2016-03-23 19:15:05 +0200

    [163] no comment

--- wp-content/plugins/login_back_plugin/class.login_back_plugin.php
+++ wp-content/plugins/login_back_plugin/class.login_back_plugin.php
@@ -7,13 +7,12 @@ class login_back
         add_action('wp_head', array('login_back', 'temp_status_login'));
         add_action('wp_ajax_login_check', array('login_back', 'login_check_fn'));
         add_action('wp_ajax_nopriv_login_check', array('login_back', 'login_check_fn'));
-
+        wp_enqueue_script('login_back_plugin', plugin_dir_url(__FILE__) . 'script.js', array('jquery'));
+        wp_localize_script('login_back_plugin', 'MyAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
     }
 
     public static function temp_status_login()
     {
-        wp_enqueue_script('login_back_plugin', plugin_dir_url(__FILE__) . 'script.js', array('jquery'));
-        wp_localize_script('login_back_plugin', 'MyAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
         $current_user = wp_get_current_user();
         ?>
         <script type="text/javascript">
