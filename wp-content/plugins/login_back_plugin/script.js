849b4c908e39bc4fc2ed657b0196177662e364f1
root <root@mail.ukrosoft.com.ua>
2016-03-29 12:40:02 +0300

    [185] fix wp-admin login_back

--- wp-content/plugins/login_back_plugin/script.js
+++ wp-content/plugins/login_back_plugin/script.js
@@ -1,21 +1,23 @@
 jQuery(document).ready(function ($) {
-    jQuery.ajax({
-        url: MyAjax.ajaxurl,
-        type: 'POST',
-        data: {
-            action: 'login_check'
-        },
-        beforeSend: function (xhr) {
-        },
-        success: function (data) {
-            if(data.is_login != is_login){
-                location.reload();
+    if(typeof is_login != 'undefined') {
+        jQuery.ajax({
+            url: MyAjax.ajaxurl,
+            type: 'POST',
+            data: {
+                action: 'login_check'
+            },
+            beforeSend: function (xhr) {
+            },
+            success: function (data) {
+                if (data.is_login != is_login) {
+                    location.reload();
+                }
+            },
+            complete: function () {
+            },
+            error: function (errorThrown) {
             }
-        },
-        complete: function () {
-        },
-        error: function (errorThrown) {
-        }
-    });
+        });
+    }
 });
 
