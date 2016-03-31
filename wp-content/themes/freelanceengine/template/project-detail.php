e7dcf2af074aa1fc0de567e9b0a3197d31140854
root <root@mail.ukrosoft.com.ua>
2016-03-31 19:10:05 +0300

    [187] 85697724 : double notification

--- wp-content/themes/freelanceengine/template/project-detail.php
+++ wp-content/themes/freelanceengine/template/project-detail.php
@@ -253,30 +253,3 @@ if ($project_status == 'publish') {
         <div class="clearfix"></div>
     </div><!-- tab-content-project !-->
 </div>  <!--col-md-12 !-->
-
-<?php
-// TODO: delete this
-?>
-<!--Modal Window For Deleting Bid!-->
-<!--<div class="modal modal-vcenter fade in" id="modal-deleting-bid">-->
-<!--    <div class="modal-dialog top-margin">-->
-<!--        <div class="modal-content" style="margin-top:150px; height:250px;">-->
-<!--            <div class="modal-header">-->
-<!--                <button type="button" class="close" data-dismiss="modal" aria-label="Close" aria-hidden="true"><span aria-hidden="true" style="font-size: 18px;">X</span></button>-->
-<!--                <h4 class="modal-title text-center text-color-popup" style="font-size:30px;">--><?php // _e('NoDeleting Bid','projects-page');?><!--</h4>-->
-<!--                <hr>-->
-<!--            </div>-->
-<!--            <div class="modal-body">-->
-<!--                <h4 class="text-center" style="font-size:25px; padding-top:10px;">--><?php // _e('NoAre you sure, you want to delete the Bid?','projects-page');?><!--</h4>-->
-<!--                <div style="padding-top:25px;">-->
-<!--                    <a style="margin:10px;" href="#" class="btn btn-apply-project-item" data-dismiss="modal">-->
-<!--                        --><?php // _e('No','projects-page');?>
-<!--                    </a>-->
-<!--                    <a style="margin:10px;" rel="--><?php //echo $project->ID;?><!--" href="#" id="--><?php //echo $has_bid;?><!--" class="btn btn-apply-project-item btn-del-project" >-->
-<!--                        --><?php // _e('Delete','projects-page');?>
-<!--                    </a>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
